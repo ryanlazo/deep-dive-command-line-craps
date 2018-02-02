8456bc2604d84afcefc8e48f046b455f7de036d1
ryanlazo <ryanlazo12@gmail.com>
2018-02-02 15:13:15 -0500

    Prepared to port Craps model to other UI environments.

--- src/edu/cnm/deepdive/Craps.java
+++ src/edu/cnm/deepdive/Craps.java
@@ -1,43 +1,40 @@
 package edu.cnm.deepdive;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class Craps {
-  private int plays = 0;
-  private int wins = 0;
+
   private State state = State.COME_OUT;
   private Random rng = new Random();
+  private List<int[]> rolls = new LinkedList<>();
 
-  public int getPlays() {
-    return plays;
+  public List<int[]> getRolls() {
+    //TODO-Return a clone
+    return rolls;
   }
 
-  public int getWins() {
-    return wins;
+  protected void reset() {
+    state = State.COME_OUT;
+    rolls.clear();
   }
-
-  public State getState() {
+  public State play() {
+    reset();
+    do {
+      roll();
+    } while(state == State.POINT);
     return state;
   }
-  public void reset() {
-    wins = 0;
-    plays = 0;
-    state = State.COME_OUT;
-
-  }
-
-  public int roll() {
-    int sum = rng.nextInt(6) + rng.nextInt(6) + 2; //2 because you have 2 dice. 6 +1 for each dice.
+  protected int roll() {
+    int[] dice = {
+       1 + rng.nextInt(6),
+       1 + rng.nextInt(6),
+    };
+    int sum = dice[0] + dice[1];
+    rolls.add(dice);
     state = state.roll(sum);
-    if (state == State.WIN) {
-      wins++;
-    }
     return sum;
-  }
-
-  public void play() {
-    state = State.COME_OUT;
-    plays++;
   }
 
   public enum State {
--- src/edu/cnm/deepdive/Play.java
+++ src/edu/cnm/deepdive/Play.java
@@ -5,23 +5,37 @@ import java.util.Scanner;
 public class Play {
 
   public static void main(String[] args) {
+    int autoPlays = (args.length > 0) ? Integer.parseInt(args[0]) : 0;
     Craps craps = new Craps();
+    Scanner scanner = new Scanner(System.in);
+    int wins = 0;
+    int plays = 0;
     do {
-      craps.play();
-      do {
-        int diceSum = craps.roll();
-        System.out.printf("Roll = %d%n", diceSum);
-      } while(craps.getState() != Craps.State.WIN && craps.getState() != Craps.State.LOSS);
-      System.out.printf("Result = %s. Total plays = %d. Total wins = %d.%n",
-          craps.getState().toString(), craps.getPlays(), craps.getWins());
-      System.out.println("Play Again? (Y/N) ;");
-      Scanner scanner = new Scanner (System.in);
-      String input = scanner.nextLine();
-      char firstChar = input.toLowerCase().charAt(0);
-      if (firstChar != 'y') {
-        break;
+      plays++;
+      Craps.State result = craps.play();
+      if (result == Craps.State.WIN){
+        wins++;
       }
-    } while (true);
+      if (plays >= autoPlays) {
+        for (int[] dice : craps.getRolls()) {
+          System.out.printf("%d %d%n", dice[0], dice[1]);
+        }
+        System.out.println(result);
+        System.out.printf("Plays: %d. Wins: %d. Winning percentage: %.2f%%%n",
+            plays, wins, 100.0 * wins / plays);
+        System.out.println("Play Again? [Y/n]");
+        String input = scanner.nextLine()
+            .trim()
+            .toLowerCase();
+        //input = input.trim();
+        //input = input.toLowerCase(); they are saying the same thing
 
-    }
+        if (input.length() > 0 && input.charAt(0) == 'n') {
+          break;
+        }
+      }
+
+    } while(true);
+
+}
   }
